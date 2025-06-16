496a8afdad3c0016cebc2a6238140f53e75c8419
AlissonVinic <alissonvincns@gmail.com>
2025-06-16 19:46:26 -0300

    Implementação da classe Conta
    Tinha ido vazia no primeiro commit

--- src/main/Entities/Conta.java
+++ src/main/Entities/Conta.java
@@ -1,4 +1,66 @@
+package main.Entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Conta {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column
+	private String nome;
+	@Column
+	private String cpf;
+	@Column
+	private String endereco;
+	
+	//getters e setters
+	
+	public long getId() {
+        return id;
+    }
+	
+	public void setId(long id) {
+        this.id = id;
+    }
 
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        if(!(nome.isBlank())) this.nome = nome;
+        else this.nome = "DEFAULT";
+       
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    
+    public void setCpf(String cpf) {
+        if(!(cpf.isBlank())) this.cpf = cpf; 
+        else this.cpf = "DEFAULT";   
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }   
+
+    public void setEndereco(String endereco) {
+        if(!(endereco.isBlank())) this.endereco = endereco;
+        else this.endereco = "DEFAULT";
+    }
+    
+    //construtores
+    
+    public Conta(Stirng nome, String cpf, String endereco) {
+    	setNome(nome);
+    	setCpf(cpf);
+    	setEndereco(endereco);
+    }
+    
+    public Conta() {
+    	
+    }      
 }
