2e5714f165bec4dc5e4e2c7270ed2a55a932821d
Alisson Vinícius <alissonvincns@gmail.com>
2025-06-16 19:26:36 -0300

    Add/Conta*/Refatoração/Dono/HerançaConta_dono
    Add:
             Conta * (Falta implementação)
    
    Refatoração:
                           Dono
    
    Heranca:
                    Conta ->dono

--- src/main/Entities/Dono.java
+++ src/main/Entities/Dono.java
@@ -1,42 +1,43 @@
 package main.Entities;
+import java.util.List;
+import java.util.ArrayList;
 
-public class Dono {
-    private String nome;
-    private String cpf;
-    private String endereco;
+
+public class Dono extends Conta{
+	
+	//construtor
 
     public Dono(String nome, String cpf, String endereco) {
-        setNome(nome);
-        setCpf(cpf);
-        setEndereco(endereco);
+        super(nome, cpf, endereco);
     }
-
+    
     public Dono() {
+    	super();
+    };
+    
+  //métodos da classe
+    
+    public Dono cadastrarDono(Dono dono) {
+    	return dono;
+    }
+    
+    public Dono alterarDono(Dono dono) {
+    	return dono;
+    }
+    
+    public Dono excluirDono(Dono dono) {
+    	return dono;
+    }
+    
+    public List<Dono> buscarTodosDono() {
+    	return new ArrayList<>();
+    }
+    
+    public List<Dono> buscarPorNome(String nome) {
+    	return new ArrayList<>();
     }
 
-    public String getNome() {
-        return nome;
+    public List<Dono> buscarPorObra(String obra){
+    	return new ArrayList<>();
     }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-
 }
