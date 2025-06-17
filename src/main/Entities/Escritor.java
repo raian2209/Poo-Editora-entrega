f7aead84aee60776eacbc8870785241454b9b13e
Isaul Felipe <isaulfelipe5@gmail.com>
2025-06-17 15:42:13 -0300

    refactoring of methods

--- src/main/Entities/Escritor.java
+++ src/main/Entities/Escritor.java
@@ -2,12 +2,10 @@ package main.Entities;
 
 import java.util.UUID;
 
-public class Escritor {
-    private long id;
-    private String nome;
-    private String cpf;
-    private String endereco;
+public class Escritor extends Conta {
+    private List<Obra> obras;
 
+    // construtores
     public Escritor(String nome, String cpf, String endereco) {
         setNome(nome);
         setCpf(cpf);
@@ -17,44 +15,7 @@ public class Escritor {
     public Escritor() {
     }
 
-
-    public long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setNome(String nome) {
-         if(!(nome.isBlank())) this.nome = nome; 
-        else this.nome = "DEFAULT";  
-    }
-
-    public void setCpf(String cpf) {
-         if(!(cpf.isBlank())) this.cpf = cpf; 
-        else this.cpf = "DEFAULT";  
-    }
-
-    public void setEndereco(String endereco) {
-         if(!(endereco.isBlank())) this.endereco = endereco; 
-        else this.endereco = "DEFAULT";  
-    }
-
-
-
+    // metodos
     public Escritor cadastrarEscritor(Escritor escritor){
 
         System.out.println("Tentando conexão do banco");
@@ -103,56 +64,16 @@ public class Escritor {
         return escritor;
     }
 
-
-
-    // ESTA VOID MAS O RETORNO É ResultSet
-    public void buscarTodosEscritor(){
-
-        System.out.println("Tentando conexão do banco");
-
-        System.out.println("Montando query ");
-
-        System.out.println("preparando statment ");
-
-        System.out.println("Inserindo valores na query");
-
-        System.out.println("EXECUTANDO");
-
-
-
+    public List<Escritor> buscarTodosEscritor(){
+        return new ArrayList<>();
     }
 
-    // Retorno é ResultSet(BANCO) ou List<Escritor>
-    public void buscarPorNome(String string){
-
-        System.out.println("Tentando conexão do banco");
-
-        System.out.println("Montando query ");
-
-        System.out.println("preparando statment ");
-
-        System.out.println("Inserindo valores na query");
-
-        System.out.println("EXECUTANDO");
-
-
-
+    public List<Escritor> buscarPorNome(String string){
+        return new ArrayList<>();
     }
 
-
-    // Retorno é ResultSet(BANCO) ou List<Escritor>
-    public void buscarPorObra(String string){
-
-        System.out.println("Tentando conexão do banco");
-
-        System.out.println("Montando query ");
-
-        System.out.println("preparando statment ");
-
-        System.out.println("Inserindo valores na query");
-
-        System.out.println("EXECUTANDO");
-
+    public List<Escritor> buscarPorObra(String string){
+        return new ArrayList<>();
     }
 
 
