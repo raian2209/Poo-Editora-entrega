a03133eba3bce7f6a728c3387149e5ca07c57a19
Isaul Felipe <isaulfelipe5@gmail.com>
2025-06-17 13:35:01 -0300

    Update Avaliador.java
    Refatoração:
                           Avaliador
    
    Heranca:
                    Conta ->Avaliador

--- src/main/Entities/Avaliador.java
+++ src/main/Entities/Avaliador.java
@@ -2,160 +2,41 @@ package main.Entities;
 
 import java.util.UUID;
 
-public class Avaliador {
-    private long id;
-    private String nome;
-    private String cpf;
-    private String endereco;
+public class Avaliador extends Conta {
 
+
+    // construtor
     public Avaliador(String nome, String cpf, String endereco) {
-        setNome(nome);
-        setCpf(cpf);
-        setEndereco(endereco);
+        super(nome, cpf, endereco);
     }
 
     public Avaliador() {
+        super();
     }
 
-    public long getId() {
-        return id;
+    // métodos da classe
+    public Avaliador cadastrarAvaliador(Avaliador avaliador) {
+    	return avaliador;
     }
 
-    public String getNome() {
-        return nome;
+    public Avaliador alterarAvaliador(Avaliador avaliador) {
+    	return avaliador;
     }
 
-    public String getCpf() {
-        return cpf;
+    public Avaliador excluirAvaliador(Avaliador avaliador) {
+    	return avaliador;
     }
 
-    public String getEndereco() {
-        return endereco;
+    public List<Avaliador> buscarTodosAvaliador() {
+    	return new ArrayList<>();
+    }
+    
+    public List<Avaliador> buscarPorNome(String nome) {
+    	return new ArrayList<>();
     }
 
-
-    public void setId(long id) {
-        this.id = id;
+    public List<Avaliador> buscarPorObra(String obra){
+    	return new ArrayList<>();
     }
-
-    public void setNome(String nome) {
-        if(!(nome.isBlank())) this.nome = nome;
-        else this.nome = "DEFAULT";
-       
-    }
-
-    public void setCpf(String cpf) {
-        if(!(cpf.isBlank())) this.cpf = cpf; 
-        else this.cpf = "DEFAULT";   
-    }
-
-    public void setEndereco(String endereco) {
-        if(!(endereco.isBlank())) this.endereco = endereco;
-        else this.endereco = "DEFAULT";
-        
-    }
-
- 
-    public Avaliador cadastrarAvaliador(Avaliador avaliador){
-
-        System.out.println("Tentando conexão do banco");
-
-        System.out.println("Montando query ");
-
-        System.out.println("preparando statment ");
-
-        System.out.println("Inserindo valores na query");
-
-        System.out.println("EXECUTANDO");
-
-
-        return avaliador;
-    }
-
-    public Avaliador alterarAvaliador(Avaliador avaliador){
-
-        System.out.println("Tentando conexão do banco");
-
-        System.out.println("Montando query ");
-
-        System.out.println("preparando statment ");
-
-        System.out.println("Inserindo valores na query");
-
-        System.out.println("EXECUTANDO");
-
-
-        return avaliador;
-    }
-
-    public Avaliador exluirAvaliador(Avaliador avaliador){
-
-        System.out.println("Tentando conexão do banco");
-
-        System.out.println("Montando query ");
-
-        System.out.println("preparando statment ");
-
-        System.out.println("Inserindo valores na query");
-
-        System.out.println("EXECUTANDO");
-
-
-        return avaliador;
-    }
-
-
-
-    // ESTA VOID MAS O RETORNO É ResultSet
-    public void buscarTodosAvaliador(){
-
-        System.out.println("Tentando conexão do banco");
-
-        System.out.println("Montando query ");
-
-        System.out.println("preparando statment ");
-
-        System.out.println("Inserindo valores na query");
-
-        System.out.println("EXECUTANDO");
-
-
-
-    }
-
-    // Retorno é ResultSet(BANCO) ou List<Avaliador>
-    public void buscarPorNome(String string){
-
-        System.out.println("Tentando conexão do banco");
-
-        System.out.println("Montando query ");
-
-        System.out.println("preparando statment ");
-
-        System.out.println("Inserindo valores na query");
-
-        System.out.println("EXECUTANDO");
-
-
-
-    }
-
-
-    // Retorno é ResultSet(BANCO) ou List<Avaliador>
-    public void buscarPorObra(String string){
-
-        System.out.println("Tentando conexão do banco");
-
-        System.out.println("Montando query ");
-
-        System.out.println("preparando statment ");
-
-        System.out.println("Inserindo valores na query");
-
-        System.out.println("EXECUTANDO");
-
-    }
-
-
 
 }
