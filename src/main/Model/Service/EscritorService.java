5323354c5564f755bb2d1fa2a297754c136ec874
Isaul Felipe <isaulfelipe5@gmail.com>
2025-06-23 15:16:37 -0300

    Update EscritorService.java
    Implementado :
                                   Foram implementados os métodos em: EscritorService

--- src/main/Model/Service/EscritorService.java
+++ src/main/Model/Service/EscritorService.java
@@ -1,23 +1,33 @@
 package main.Model.Service;
 
-
 import main.Entities.Escritor;
 import main.Model.Dao.EscritorDAO;
 
 public class EscritorService {
 
-    private EscritorDAO EscritorDAO;
+    private EscritorDAO escritorDAO = new EscritorDAO();
 
-    public void  criarEscritor(Escritor escritor){
-
+    public void criarEscritor(Escritor escritor) {
+        if (escritorDAO.buscarPorCPF(escritor.getCpf()) == null) {
+            escritorDAO.salvar(escritor);
+        } else {
+            System.out.println("Já existe um escritor com esse cpf");
+        }
     }
 
-    public void  deletarEscritor(Escritor escritor){
-
+    public void deletarEscritor(Escritor escritor) {
+        if (escritorDAO.buscarPorCPF(escritor.getCpf()) == null) {
+            System.out.println("Não existe um escritor com esse cpf");
+        } else {
+            escritorDAO.deletar(escritor);
+        }
     }
 
-    public void  atualisarEscritor(Escritor escritor){
-
+    public void atualisarEscritor(Escritor escritor) {
+        if (escritorDAO.buscarPorCPF(escritor.getCpf()) == null) {
+            System.out.println("Não existe um escritor com esse cpf");
+        } else {
+            escritorDAO.salvar(escritor);
+        }
     }
-
 }
