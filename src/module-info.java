744f764a1eb4fc97fcbc890fef5a1800613a1e28
raian2209 <raimundo.neto45435@alunos.ufersa.edu.br>
2025-07-10 10:37:34 -0300

    configuração para rodar o java FX

---
+++ src/module-info.java
@@ -0,0 +1,20 @@
+module POO.Editora.entrega {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+
+    opens main.Entities to org.hibernate.orm.core;
+
+    opens View to javafx.fxml;
+    exports View;
+}
